1e89dbf26e5eafcc02017714808279c6bd794a10
Khaelano Abroor Maulana <contact.khaelano@gmail.com>
2024-03-21 09:21:34 +0700

    Remove isIncludeDriver method and change includeDriver attribute access modifier to public

--- src/com/car/Car.java
+++ src/com/car/Car.java
@@ -4,19 +4,14 @@ public class Car {
     public String brand;
     public String model;
     public String color;
+    public boolean includeDriver;
     protected String licensePlate;
-    protected boolean includeDriver;
 
     public final int FEE = 0;
     public final int CAPACITY = 0;
 
     public Car(String licensePlate) {
         this.licensePlate = licensePlate;
-    }
-
-    public boolean isDriverIncluded(boolean includeDriver) {
-        this.includeDriver = includeDriver;
-        return this.includeDriver;
     }
 
     public String getLicensePlate() {
