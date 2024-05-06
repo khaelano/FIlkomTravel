9b4ce5de1352c737d48bcb57cd4412c615efa5d6
lutfiiaziz <muhammadlutfiaziz719@gmail.com>
2024-05-06 12:15:55 +0700

    Class Cashback and Shiiping

--- src/com/payment/CashbackPromo.java
+++ src/com/payment/CashbackPromo.java
@@ -1,5 +1,43 @@
 package com.payment;
 
-public class CashbackPromo {
+import com.user.User;
+
+public class CashbackPromo extends Promotion{
+
+    @Override
+    public int calculateShippingDiscount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isCustomerEligible(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean isMinimumPriceEligible(Order order) {
+        return false;
+    }
+
+    @Override
+    public boolean isShippingDiscountEligible(Order order) {
+        return false;
+    }
+
+    @Override
+    public int totalCashback() {
+        Order order;
+        final int BATAS_DUMMY = 1_000_000;
+        if (order.calulateCharges() >= BATAS_DUMMY) {
+            
+        } 
+        return 0;
+    }
+
+    @Override
+    public int totalDiscount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
--- src/com/payment/ShippingDiscount.java
+++ src/com/payment/ShippingDiscount.java
@@ -1,5 +1,41 @@
 package com.payment;
 
-public class ShippingDiscount {
+import com.user.User;
 
+public class ShippingDiscount extends Promotion{
+
+    @Override
+    public int calculateShippingDiscount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isCustomerEligible(User user) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isMinimumPriceEligible(Order order) {
+        System.out.println("DUMMY");
+        return false;
+    }
+
+    @Override
+    public boolean isShippingDiscountEligible(Order order) {
+        System.out.println("DUMMY");
+        return false;
+    }
+
+    @Override
+    public int totalCashback() {
+        return 0;
+    }
+
+    @Override
+    public int totalDiscount() {
+        return 0;
+    }
+    
 }
