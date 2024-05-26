07b3c7f5744a869e0e4b09407c90b5230d86b099
Khaelano Abroor Maulana <contact.khaelano@gmail.com>
2024-05-26 10:25:43 +0700

    Add new abstract class Vehicle (this is the parent class of Car and Bike).

---
+++ src/components/car/Vehicle.java
@@ -0,0 +1,40 @@
+/*
+ * 235150200111051 Khaelano Abroor Maulana
+ * 235150200111057 Arza Marevi Bangun
+ * 235150207111058 Muhammad Lutfi Aziz
+ * 235150207111059 Nabiel Tatra Edy Firdaus
+ */
+
+package components.car;
+
+public abstract class Vehicle {
+    private String vehicleID;
+    private String vehicleName;
+    private String licenseNumber;
+    private long rentFee;
+
+    public Vehicle(
+        String vehicleID,
+        String vehicleName,
+        String licenseNumber,
+        long rentFee
+    ) {
+        this.vehicleID = vehicleID;
+        this.vehicleName = vehicleName;
+        this.licenseNumber = licenseNumber;
+        this.rentFee = rentFee;
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+    public String getVehicleName() {
+        return vehicleName;
+    }
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+    public long getRentFee() {
+        return rentFee;
+    }
+}
