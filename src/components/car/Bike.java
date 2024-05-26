3ee33ebae79c0ee5d38fda1477c36df644eecebc
Khaelano Abroor Maulana <contact.khaelano@gmail.com>
2024-05-26 10:25:56 +0700

    Add new class Bike

---
+++ src/components/car/Bike.java
@@ -0,0 +1,19 @@
+/*
+ * 235150200111051 Khaelano Abroor Maulana
+ * 235150200111057 Arza Marevi Bangun
+ * 235150207111058 Muhammad Lutfi Aziz
+ * 235150207111059 Nabiel Tatra Edy Firdaus
+ */
+
+package components.car;
+
+public class Bike extends Vehicle{
+    public Bike(
+        String vehicleID,
+        String vehicleName,
+        String licenseNumber,
+        long rentFee
+    ) {
+        super(vehicleID, vehicleName, licenseNumber, rentFee);
+    }
+}
